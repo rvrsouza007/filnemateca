b953e16f7f5c5e1543d42733283b1fdb967b2a84
Rhuan <rhuanramos762@gmail.com>
2022-03-24 16:15:32 -0300

    dando req e res na entrada e na saida

--- middleware/middlewareGlobal.js
+++ middleware/middlewareGlobal.js
@@ -5,6 +5,8 @@ module.exports = (req, res, next) => {
     if (hora<8 || hora >= 20){
         res.send ("ja ta tarde ... va para casa!")
     } else {
+        console.log("A requisição está indo...");
         next();
+        console.log("A resposta está voltando...");
     }
 }
